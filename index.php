416fa152bb71e9d177ae1ae3d39969981f250489
Bibit Kukuh Wicaksono <bibitkukuh00@gmail.com>
2022-11-15 21:07:19 +0700

    perintah tugas4

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
-$nama = "Bibit"
-?>
-<!DOCTYP<html>
-<html>
-<head>
-<title>Belajar PHP</title>
-</head>
-<body>
+$nama = "Bibit";
 
-<h1>Hello, Selamat Datang <?php echo $nama;?></h1>
-</body>
-</html>
+echo "Hallo, selamat datang $nama </br>";
+echo 'Hallo, selamat datang $nama';
+?>
