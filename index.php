d1d05392f5851fbcaa33041b54d2044b3542eac1
Bibit Kukuh Wicaksono <bibitkukuh00@gmail.com>
2022-11-15 21:04:45 +0700

    perintah tugas3

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <?php
-$nama = "Bibit";
+$nama = "Bibit"
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYP<html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Belajar PHP</title>
+<title>Belajar PHP</title>
 </head>
 <body>
 
-<h1>Hallo, Selamat Datang <?php echo $nama; ?></h1>";
-
+<h1>Hello, Selamat Datang <?php echo $nama;?></h1>
 </body>
 </html>
